34752c8c4c6e2bcf151ed89520fc6ed78104630c
calvin9150 <wonbae9150@gmail.com>
2021-06-11 20:53:19 +0900

    Add responsive greed

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -18,12 +18,31 @@ const AppLayout = ({ children }) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
         </Menu.Item>
       </Menu>
-      {children}
+      <Row>
+        <Col xs={24} md={6}>
+          왼쪽 메뉴
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a
+            href="https://www.naver.com"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            이동
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
