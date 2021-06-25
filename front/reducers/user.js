1f24e31f616f08a920c357c3f78c6a458ee99a9d
calvin9150 <wonbae9150@gmail.com>
2021-06-26 01:18:32 +0900

    Set redux-saga in store

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -6,28 +6,71 @@ export const initialState = {
 };
 
 export const loginAction = (data) => {
-  console.log(data);
+  return (dispatch, getState) => {
+    const state = getState();
+    dispatch(loginRequestAction());
+    axios
+      .post("/api/login")
+      .then((res) => {
+        dispatch(loginSuccessAction(res.data));
+      })
+      .catch((error) => {
+        dispatch(loginFailureAction(err));
+      });
+  };
+};
+
+export const loginRequestAction = (data) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     data,
   };
 };
 
-export const logoutAction = (data) => {
+export const loginSuccessAction = (data) => {
   return {
-    type: "LOG_OUT",
+    type: "LOG_IN_SUCCESS",
+    data,
+  };
+};
+
+export const loginFailureAction = (data) => {
+  return {
+    type: "LOG_IN_FAILURE",
+    data,
+  };
+};
+
+export const logoutSuccessAction = (data) => {
+  return {
+    type: "LOG_OUT_REQUEST",
+    data,
+  };
+};
+
+export const logoutRequestAction = (data) => {
+  return {
+    type: "LOG_OUT_SUCCESS",
+    data,
+  };
+};
+
+export const logoutFailureAction = (data) => {
+  return {
+    type: "LOG_OUT_FAILURE",
+    data,
   };
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "LOG_IN":
+    case "LOG_IN_REQUEST":
       return {
         ...state,
         isLoggedIn: true,
         me: action.data,
       };
-    case "LOG_OUT":
+    case "LOG_OUT_REQUEST":
       return {
         ...state,
         isLoggedIn: false,
